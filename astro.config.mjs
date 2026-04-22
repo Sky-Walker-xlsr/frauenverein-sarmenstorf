40d8b47b55add53401e52d412ca1a527e4782e0d
Yannick Salm <yannicksalm07@gmail.com>
2026-04-22 08:08:56 +0000

    rollback ohne cloudflare ding bums

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
-  // deine echte Domain
   site: 'https://frauenverein-sarmenstorf.ch',
-  output: 'server',
-  adapter: cloudflare()
 });
